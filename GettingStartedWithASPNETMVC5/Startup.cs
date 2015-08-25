f94ec179b32d90454beeafc2c11ebf1d4fc76aae
jophy <jophyjob@gmail.com>
2015-08-26 02:48:56 +0530

    Adding ASP.net MVC project with Database connection

---
+++ GettingStartedWithASPNETMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GettingStartedWithASPNETMVC5.Startup))]
+namespace GettingStartedWithASPNETMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
